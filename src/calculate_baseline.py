662b9d87447d4497d78a907d5134693c2386171b
timtimtim3 <68196410+timtimtim3@users.noreply.github.com>
2024-12-15 12:09:22 +0100

    add seed to DataClass (for isic) so data is in same fixed order as during training

--- src/calculate_baseline.py
+++ src/calculate_baseline.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     checkpoint_obj = torch.load(best_model_path, map_location=device)
     checkpoint = ModelCheckpoint(**checkpoint_obj)
 
-    train_dataset = DatasetClass(split="train", label_noise=args.label_noise)
+    train_dataset = DatasetClass(split="train", label_noise=args.label_noise, seed=args.seed)
     sample_info = get_sample_info(train_dataset)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False)
 
